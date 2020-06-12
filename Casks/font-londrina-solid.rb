afef8a065686b655c17208c78999a1d7fe2247e9
Aidan Feldman <aidan.feldman@gmail.com>
2020-06-12 14:21:20 -0400

    ensure URL on google fonts match

--- Casks/font-londrina-solid.rb
+++ Casks/font-londrina-solid.rb
@@ -3,7 +3,7 @@ cask 'font-londrina-solid' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
+  # github.com/google/fonts was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts.git'
   name 'Londrina Solid'
   homepage 'https://fonts.google.com/specimen/Londrina+Solid'
