a615bebbe8fdc560852d746627622bddb1c5084e
Aidan Feldman <aidan.feldman@gmail.com>
2020-06-12 03:08:32 -0400

    switch to git URLs for google fonts

--- Casks/font-londrina-solid.rb
+++ Casks/font-londrina-solid.rb
@@ -3,14 +3,12 @@ cask 'font-londrina-solid' do
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/londrinasolid',
-      using:      :svn,
-      trust_cert: true
+  url 'https://github.com/google/fonts.git'
   name 'Londrina Solid'
   homepage 'https://fonts.google.com/specimen/Londrina+Solid'
 
-  font 'LondrinaSolid-Black.ttf'
-  font 'LondrinaSolid-Light.ttf'
-  font 'LondrinaSolid-Regular.ttf'
-  font 'LondrinaSolid-Thin.ttf'
+  font 'ofl/londrinasolid/LondrinaSolid-Black.ttf'
+  font 'ofl/londrinasolid/LondrinaSolid-Light.ttf'
+  font 'ofl/londrinasolid/LondrinaSolid-Regular.ttf'
+  font 'ofl/londrinasolid/LondrinaSolid-Thin.ttf'
 end
